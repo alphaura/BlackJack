1b8f1384866cf20a00f44532a03febb8a6ed4fdb
alphaura <kylemagri98@gmail.com>
2023-10-13 23:02:17 +0200

    Working on jS

---
+++ index.js
@@ -0,0 +1,27 @@
+let isAlive = false
+let hasBlackJack = false
+let cards = []
+let startGame = false
+let dealerCountEl = document.getElementById("dealer-count")
+let dealer = document.getElementById("dealer")
+let playerCountEl = document.getElementById("player-count")
+let player = document.getElementById("player")
+
+
+
+
+
+
+
+let getRandomCard() {
+    let randomCard = Math.floor ( Math.random() * 10 ) + 1
+}
+
+
+function hitMe() {
+
+}
+
+function stay() {
+
+}

eef749b45e73a99aac67a3bab0978c17a20cf4a9
alphaura <120915801+alphaura@users.noreply.github.com>
2023-10-14 14:41:25 +0000

    Added more functionality to the jS side

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let isAlive = false
 let hasBlackJack = false
-let cards = []
+let dealerCards = []
+let playerCards = []
 let startGame = false
 let dealerCountEl = document.getElementById("dealer-count")
 let dealer = document.getElementById("dealer")
 let playerCountEl = document.getElementById("player-count")
-let player = document.getElementById("player")
+let player = document.getElementById("player")y
+let buttonMessage = ""
+let triggerMessage = ""
+let triggerSection = document.getElementById("trigger-section")
 
-
-
+dealerCountEl = 0
+playerCountEl = 0
 
 
 
@@ -17,9 +21,53 @@ let getRandomCard() {
     let randomCard = Math.floor ( Math.random() * 10 ) + 1
 }
 
+function gameStart() {
+    let dealerFirstCard = getRandomCard()
+    let playerFirstCard = getRandomCard()
+    let playerSecondCard = getRandomCard()
+    dealerCards = [dealerFirstCard]
+    playerCards = [playerFirstCard, playerSecondCard]
+    dealerCountEl = dealerFirstCard
+    playerCountEl = playerFirstCard + playerSecondCard
+    dealer = dealerFirstCard
+    player = playerFirstCard + " " + playerSecondCard
+}
+
+function gamePlay() {
+    for (let i = 0; i > dealerCards; i++) {
+        dealer.textContent = dealerCards[i] + " "
+    }
+
+    for ( let x = 0; x > playerCards; x++) {
+        player.textContent = playerCards[x] + " "
+    }
+
+    if ( playerCountEl > 21 && dealerCountEl > 21 ) {
+        triggerMessage = "What’s your move?"
+    } else if ( playerCountEl < 21 && dealerCountEl > 21 ) {
+        triggerMessage = "You’re out!"
+    } else if ( playerCountEl === 21 && ( dealerCountEl > 21 || dealerCountEl < 21 ) ) {
+        triggerMessage = "You got blackjack!"
+    } else if ( playerCountEl === 21 && dealerCountEl === 21 ) {
+        triggerMessage = "It’s a draw!"
+    } else ( playerCountEl > 21 && dealerCountEl < 21 ) {
+        triggerMessage = "Dealer busted!"
+    }
+
+    triggerSection = triggerMessage
+}
+
 
 function hitMe() {
-
+    if (startGame === false) {
+        gameStart()
+    } else {
+        let card = getRandomCard()
+        playerCountEl += card
+        playerCards.push(card)
+        player.push(card)
+        gamePlay()
+    }
 }
 
 function stay() {
